b698bea13ff395192919f088b049827fde1ba232
Tim Olsén <skvaertill@hotmail.com>
2024-09-16 18:36:30 +0200

    Add skeleton code for all required endpoints/methods

--- api.py
+++ api.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
 db = SQLAlchemy(app)
 
-
-# Send messages 
+# Send a message to a user.
 @app.route('/messages', methods=['POST'])
 def send_message():
     pass
@@ -17,11 +16,21 @@ def send_message():
 def fetch_new_messages():
     pass
 
-
 # Delete message
 @app.route('/messages/<int:id>', methods=['DELETE'])
 def delete_message(id):
     pass
+
+# Delete multiple messages
+@app.route('/messages', methods=['DELETE'])
+def delete_multiple_messages():
+    pass
+
+# Get ALL messages.
+@app.route('/messages', methods=['GET'])
+def fetch_messages():
+    pass
+
 
 if __name__ == '__main__':
     app.run(debug=True)
