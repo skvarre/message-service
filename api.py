00bb81283d8c86c163b81c77806411a6a46aa608
Tim Olsén <skvaertill@hotmail.com>
2024-09-17 09:00:44 +0200

    Add Message class for ORM

--- api.py
+++ api.py
@@ -6,12 +6,25 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
 db = SQLAlchemy(app)
 
-# Send a message to a user.
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender = db.Column(db.String(100), nullable=False)
+    receiver = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    is_read = db.Column(db.Boolean, nullable=False, default=False)
+
+    # For debugging purposes
+    def __repr__(self):
+        return f'<Message(id={self.id}, sender={self.sender}, receiver={self.receiver}, content={self.content}, timestamp={self.timestamp}, is_read={self.is_read})>'
+
+# Send a message to a user
 @app.route('/messages', methods=['POST'])
 def send_message():
     pass
 
-# Get NEW messages.
+
+# Get NEW messages
 @app.route('/messages/new', methods=['GET'])
 def fetch_new_messages():
     pass
@@ -33,4 +46,6 @@ def fetch_messages():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
